c3fc2e72a4a6243cd02b3946f4df01f63d49ddc0
Prithvi <prithvi3039x@gmail.com>
2026-09-15 14:36:30 +0530

    Added table maker

--- while_loop.py
+++ while_loop.py
@@ -6,12 +6,20 @@
 #         print("closing...")
 #         break
 
-i = 0
-count = 0
-while i <= 10:
-    print(i)
-    i+=2
-    count+=1
+
+# i = 0
+# count = 0
+# while i <= 10:
+#     print(i)
+#     i+=2
+#     count+=1
     
 
-print(f"the loop exexuted {count} times")
+# print(f"the loop exexuted {count} times")
+
+num = int(input("Enter a number to print its table :"))
+i=1
+while i <= 10:
+    print(f"{num} * {i} = {num * i}")
+    i+=1
+    break
