3d9fc66b8632bfdd72afaf96891ccf4b84ea0e92
Prithvi <prithvi3039x@gmail.com>
2026-09-15 14:20:11 +0530

    added even number printing and counting using while loop

--- while_loop.py
+++ while_loop.py
@@ -1,3 +1,17 @@
-while True:
-    string = input("Enter a string :")
-    print(string)
+# while True:
+#     password = input("Enter a password :")
+#     password = password.lower()
+#     print(f"{password} is not correct !!")
+#     if password == "close":
+#         print("closing...")
+#         break
+
+i = 0
+count = 0
+while i <= 10:
+    print(i)
+    i+=2
+    count+=1
+    
+
+print(f"the loop exexuted {count} times")
